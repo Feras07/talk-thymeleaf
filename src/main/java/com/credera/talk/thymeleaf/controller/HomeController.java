c4414da337094f181e53db145295c914fe3c2e0c
Justin Munn <munnja001@gmail.com>
2013-04-23 23:54:33 -0500

    initial commit

---
+++ src/main/java/com/credera/talk/thymeleaf/controller/HomeController.java
@@ -0,0 +1,25 @@
+
+package com.credera.talk.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(value = "/prototype", method = RequestMethod.GET)
+    public String getPrototype(HttpServletRequest request, Model model) {
+        model.addAttribute("message", "Welcome to the Thymeleaf Demo!");
+        return "common/index-prototype";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getHome(HttpServletRequest request, Model model) {
+        return "common/index";
+    }
+}
